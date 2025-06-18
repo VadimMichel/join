f06f1f862a761cd7388afcfd28d5848f515faa25
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-18 18:19:02 +0200

    chore: add initial project file structure

---
+++ src/app/main-pages/contacts/contact-dialog/contact-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-dialog',
+  imports: [],
+  templateUrl: './contact-dialog.component.html',
+  styleUrl: './contact-dialog.component.scss'
+})
+export class ContactDialogComponent {
+
+}
---
+++ src/app/main-pages/main-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-pages',
+  imports: [],
+  templateUrl: './main-pages.component.html',
+  styleUrl: './main-pages.component.scss'
+})
+export class MainPagesComponent {
+
+}
