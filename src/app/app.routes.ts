150f18aa699e6cf0b0ae32ceeb8b5acd7e54fb4a
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-06-19 15:43:29 +0200

    feat: set ContactsComponent as default route, restructure styles, and add Inter font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './main-pages/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    path: '',
+    component: ContactsComponent,
+  },
+];
