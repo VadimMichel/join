b3a9abe9bf5f72d7453495ddd3bb34fd69678134
VadimMichel <vadimmichel5@gmail.com>
2025-06-20 10:31:50 +0200

    feat: display firesore data in contactListComponent

--- src/app/main-pages/contacts/contact-list/contact-list.component.ts
+++ src/app/main-pages/contacts/contact-list/contact-list.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ContactDataService } from '../../contact-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
 export class ContactListComponent {
+  constructor(public contactDataService: ContactDataService){
 
+  }
 }
