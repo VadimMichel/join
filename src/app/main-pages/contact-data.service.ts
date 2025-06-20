6acd0c0b2df3ce6dd5bf4b4a50245024c8270bf7
VadimMichel <vadimmichel5@gmail.com>
2025-06-20 10:30:57 +0200

    feat: inject Firestore and add reference functions

--- src/app/main-pages/contact-data.service.ts
+++ src/app/main-pages/contact-data.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactDataService {
 
-  // Hier wird firebase injected werden
+  firestore = inject(Firestore);
+  items$ = collectionData(this.getContactRef());
 
   constructor() { }
+
+  getContactRef(){
+    return collection(this.firestore, 'contacts');
+  }
+
+  getSingleDocRef(colId:string, docId:string){
+    return doc(collection(this.firestore, colId), docId)
+  }
+
 }
