b3ab3775230f02c2759193d46a198ade91577b1d
Robin Angelé <frontend@robin4consulting.com>
2025-06-23 13:31:58 +0200

    feat: enhances contact list display with dynamic colors

--- src/app/main-pages/contacts/contact-list/contact-list.component.ts
+++ src/app/main-pages/contacts/contact-list/contact-list.component.ts
@@ -30,4 +30,15 @@ export class ContactListComponent {
       .map(word => word.charAt(0)) // Get the first character of each word
       .join(''); // Join the characters to form initials
   }
+
+  getRandomColor(name: string): string {
+    const colors = [
+      '#00BEE8', '#1FD7C1', '#6E52FF', '#9327FF', '#462F8A',
+      '#C3FF2B', '#FC71FF', '#FF4646', '#FF5EB3', '#FF745E',
+      '#FF7A00', '#FFA35E', '#FFBB2B', '#FFC701', '#FFE62B'
+    ];
+
+    const hash = name.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    return colors[hash % colors.length];
+  }
 }
