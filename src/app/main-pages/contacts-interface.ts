12280a3fdc2c7dcc58fdf708e44b11ddb46a3212
VadimMichel <vadimmichel5@gmail.com>
2025-06-24 08:12:59 +0200

    feat: Create contact interface

---
+++ src/app/main-pages/contacts-interface.ts
@@ -0,0 +1,11 @@
+export interface Contacts{
+        letter: string;
+        contacts: [
+            {
+                name: string;
+                email: string;
+                phone: string;
+            }
+        ]
+    
+}
