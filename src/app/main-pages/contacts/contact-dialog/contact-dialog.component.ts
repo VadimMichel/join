9ec8b534b775c6f26aff346b21b1580326f8f76c
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-06-24 23:30:29 +0200

    feat: add slide-in animation to ContactDialogComponent and set background color in ContactsComponent

--- src/app/main-pages/contacts/contact-dialog/contact-dialog.component.ts
+++ src/app/main-pages/contacts/contact-dialog/contact-dialog.component.ts
@@ -1,18 +1,19 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { ContactFormComponent } from './contact-form/contact-form.component';
 import { Contacts } from '../../contacts-interface';
 
 @Component({
   selector: 'app-contact-dialog',
   templateUrl: './contact-dialog.component.html',
   styleUrl: './contact-dialog.component.scss',
-  imports: [ContactFormComponent]
+  imports: [ContactFormComponent],
 })
 export class ContactDialogComponent {
   @Input() editingContact: Contacts | null = null;
   @Output() contactSubmitted = new EventEmitter<Contacts>();
   @Output() dialogClosed = new EventEmitter<void>();
 
+  animated: boolean = true;
 
   onContactSubmitted(contactData: Contacts) {
     this.contactSubmitted.emit(contactData);
@@ -25,5 +26,5 @@ export class ContactDialogComponent {
   onOverlayClick(event: MouseEvent) {
     // Close dialog when clicking on overlay
     this.onClose();
-  } 
+  }
 }
