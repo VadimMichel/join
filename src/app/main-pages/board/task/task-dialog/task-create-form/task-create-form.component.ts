2d87aa5cf8e59c5ee054bc9c025196f8cbd4dba9
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-06-30 16:31:54 +0200

    feat: add task-related components

---
+++ src/app/main-pages/board/task/task-dialog/task-create-form/task-create-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-create-form',
+  imports: [],
+  templateUrl: './task-create-form.component.html',
+  styleUrl: './task-create-form.component.scss'
+})
+export class TaskCreateFormComponent {
+
+}
---
+++ src/app/main-pages/board/task/task-dialog/task-edit-form/task-edit-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit-form',
+  imports: [],
+  templateUrl: './task-edit-form.component.html',
+  styleUrl: './task-edit-form.component.scss'
+})
+export class TaskEditFormComponent {
+
+}
