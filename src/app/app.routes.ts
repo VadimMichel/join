823ef44b688d9d755911201ac31e582b72d6e7d0
VadimMichel <vadimmichel5@gmail.com>
2025-07-01 07:42:41 +0200

    feat: add Add-Task route and link it in navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactsComponent } from './main-pages/contacts/contacts.component';
 import { ContactDetailsComponent } from './main-pages/contacts/contact-details/contact-details.component';
 import { BoardComponent } from './main-pages/board/board.component';
+import { AddTaskComponent } from './main-pages/add-task/add-task.component';
 
 export const routes: Routes = [
       {path: '', component: ContactsComponent},
       {path: 'contacts', component: ContactsComponent},
       {path: 'contacts/:id', component: ContactDetailsComponent},
-      { path: 'board', component: BoardComponent },
+      {path: 'board', component: BoardComponent},
+      {path: 'addTask', component: AddTaskComponent},
 ];
