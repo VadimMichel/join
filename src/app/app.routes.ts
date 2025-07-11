dd9e5dcfa59c7efafa29925447b5e84a38f9c255
Robin Angelé <frontend@robin4consulting.com>
2025-07-11 19:13:01 +0200

    feat: adds Legal Notice page and updates navigation
    Introduces a new Legal Notice component with detailed legal information.
    
    Updates the routing to include the Legal Notice page and modifies the navigation links accordingly.
    
    This ensures users have access to important legal information about the product.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ContactsComponent } from './main-pages/contacts/contacts.component';
 import { ContactDetailsComponent } from './main-pages/contacts/contact-details/contact-details.component';
 import { BoardComponent } from './main-pages/board/board.component';
 import { AddTaskComponent } from './main-pages/add-task/add-task.component';
+import { LegalNoticeComponent } from './legal-pages/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
       {path: '', component: BoardComponent},
@@ -10,4 +11,5 @@ export const routes: Routes = [
       {path: 'contacts/:id', component: ContactDetailsComponent},
       {path: 'board', component: BoardComponent},
       {path: 'addTask', component: AddTaskComponent},
+      {path: 'legal-notice', component: LegalNoticeComponent},
 ];
