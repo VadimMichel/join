1477fe058e7d1e4f62df40e7adbbb6f0dbba6c71
VadimMichel <vadimmichel5@gmail.com>
2025-07-12 13:58:49 +0200

    feat: change input border color on focus and toggle password icon

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  passwordInput: boolean = false;
+  emailInput: boolean = false;
+  showpassword: boolean = false;
 
 }
