a7a7def44dcfcf168616c96c074bb4094dc75b0f
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-12 15:17:52 +0200

    feat: implement basic sign-up logic using createUserWithEmailAndPassword in AuthenticationService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,5 +10,20 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-1f201", appId: "1:423152425379:web:e6bb2be18bf7c3a3e3e216", storageBucket: "join-1f201.firebasestorage.app", apiKey: "AIzaSyAfeP9cN-a0hSo3PxQaPwJHhSE99i40xsQ", authDomain: "join-1f201.firebaseapp.com", messagingSenderId: "423152425379" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-1f201',
+        appId: '1:423152425379:web:e6bb2be18bf7c3a3e3e216',
+        storageBucket: 'join-1f201.firebasestorage.app',
+        apiKey: 'AIzaSyAfeP9cN-a0hSo3PxQaPwJHhSE99i40xsQ',
+        authDomain: 'join-1f201.firebaseapp.com',
+        messagingSenderId: '423152425379',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/auth/services/authentication.service.ts
+++ src/app/auth/services/authentication.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {
+  createUserWithEmailAndPassword,
+  Auth,
+  UserCredential,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(private auth: Auth) {}
 
-  constructor() { }
+  // #region SignUp
+  async signUp(
+    email: string,
+    password: string
+  ): Promise<UserCredential | void> {
+    try {
+      return await createUserWithEmailAndPassword(this.auth, email, password);
+    } catch (error: unknown) {
+      console.error(error);
+      throw error;      
+    }
+  }
+
+  // #endregion
 }
