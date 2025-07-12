eea7d9ee2dac3ebecb85c2b24b432a38500bbf88
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-12 18:32:38 +0200

    feat: throw descriptive errors in AuthenticationService and handle them in RegisterComponent

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,17 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-register',
   imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  // #region Properties
   userName: string = '';
-  email : string = '';
-  password : string = '';
-  confirmPassword : string = '';
-  confirmprivacyPolicy : boolean = false;
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  confirmprivacyPolicy: boolean = false;
+  errorMessage: string = '';
+  // #endregion
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  // #region Auth Methods
+  async createNewAccount() {
+    try {
+      await this.authenticationService.signUp(this.email, this.password);
+    } catch (error) {
+      this.errorMessage = (error as Error).message;
+      console.log(this.errorMessage);
+    }
+  }
+  // #endregion
 }
--- src/app/auth/services/authentication.service.ts
+++ src/app/auth/services/authentication.service.ts
@@ -15,25 +15,37 @@ import {
   providedIn: 'root',
 })
 export class AuthenticationService {
+  // #region Properties
   /**
    * Angular EnvironmentInjector for running code in the correct injection context.
    */
   private readonly injector = inject(EnvironmentInjector);
+  // #endregion
 
   constructor(private auth: Auth) {}
 
-  // #region SignUp & SignIn
+  // #region Sign Up
   async signUp(email: string, password: string): Promise<UserCredential> {
     try {
       return await runInInjectionContext(this.injector, () =>
         createUserWithEmailAndPassword(this.auth, email, password)
       );
     } catch (error: unknown) {
-      console.error("Can't create user ", error);
-      throw error;
+      const err = error as { code?: string; message?: string };
+      if (err.code === 'auth/email-already-in-use') {
+        console.error("Can't create user ", error);
+        throw new Error('This email is already taken');
+      } else if (err.code === 'auth/invalid-email') {
+        throw new Error('This email is invalid');
+      } else {
+        console.error("Can't create user ", error);
+        throw new Error('Something went wrong');
+      }
     }
   }
+  // #endregion
 
+  // #region Sign In
   async signIn(email: string, password: string): Promise<UserCredential> {
     try {
       return await runInInjectionContext(this.injector, () =>
