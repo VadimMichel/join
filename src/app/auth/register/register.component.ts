8b76532fcfff856ba305107c9e390818396ee5e3
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-15 09:44:29 +0200

    feat: add contact creation to onSignUp process

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { ContactDataService } from '../../main-pages/shared-data/contact-data.service';
 
 @Component({
   selector: 'app-register',
@@ -26,12 +27,19 @@ export class RegisterComponent {
   manualChange: boolean = false;
   // #endregion
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+  constructor(private authenticationService: AuthenticationService, private router: Router, private contactDataService: ContactDataService) {}
 
   // #region Auth Methods
-  async createNewAccount() {
+  async onSignUp() {
     try {
       await this.authenticationService.signUp(this.email, this.password);
+      await this.contactDataService.addContact(
+        {
+          name: this.userName,
+          email: this.email,
+          phone: "",
+        }
+      );
       this.router.navigate(['/auth/login']); // Sobald vorhanden zu Summary navigieren
     } catch (error) {
       this.errorMessage = (error as Error).message;
