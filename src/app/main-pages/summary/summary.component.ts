5f9878596c2a96d1c719297c7fc3f00658134f98
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-16 16:02:28 +0200

    feat: add logic to count specific tasks in SummaryComponent

--- src/app/main-pages/summary/summary.component.ts
+++ src/app/main-pages/summary/summary.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TaskDataService } from '../shared-data/task-data.service';
+import { Observable } from 'rxjs';
+import { BoardColumn } from '../shared-data/task.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrl: './summary.component.scss',
 })
 export class SummaryComponent {
+  columns$!: Observable<BoardColumn[]>;
 
+  constructor(public taskDataService: TaskDataService) {}
+
+  // #region Methodes
+
+  ngOnInit() {
+    this.columns$ = this.taskDataService.getBoardColumns();
+  }
+
+  isMobile(): boolean {
+    return window.innerWidth <= 768;
+  }
+
+  getNextDeadline(columns: BoardColumn[]) {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: '2-digit' };
+
+    const urgentTasksWithDueDate = columns
+      .flatMap((col) => col.tasks)
+      .filter((task) => task.priority === 'urgent' && task.status !== 'done')
+      .filter((task) => task.dueDate !== undefined)
+      .sort((a, b) => a.dueDate!.getTime() - b.dueDate!.getTime());
+
+    if (urgentTasksWithDueDate.length > 0) {
+      return urgentTasksWithDueDate[0].dueDate!.toLocaleDateString("en-US", options);
+    } else {
+      return "---"
+    }
+  }
+
+  countUrgentOpenTasks(columns: BoardColumn[]): number {
+    return columns
+      .flatMap((col) => col.tasks)
+      .filter((task) => task.priority === 'urgent' && task.status !== 'done')
+      .length;
+  }
+
+  countOpenTaskWithStatus(columns: BoardColumn[], status: string): number {
+    if (status === 'incomplete') {
+      return columns
+        .flatMap((col) => col.tasks)
+        .filter((task) => task.status !== 'done').length;
+    } else {
+      return columns
+        .flatMap((col) => col.tasks)
+        .filter((task) => task.status === status).length;
+    }
+  }
+  // #endregion
 }
