61402a72f49fccc886df87772970fa1ce8e10fc6
Robin Angelé <frontend@robin4consulting.com>
2025-07-23 12:15:07 +0200

    refactor: removes unnecessary whitespace in unauth guard

--- src/app/auth/guards/unauth.guard.ts
+++ src/app/auth/guards/unauth.guard.ts
@@ -14,7 +14,6 @@ import { AuthenticationService } from '../services/authentication.service';
 export const unauthGuard: CanActivateFn = (route, state) => {
   const authenticationService = inject(AuthenticationService);
   const router = inject(Router);
-
   if (!authenticationService.isAuthenticated() || authenticationService.isGuestUser()) {
     return true;
   } else {
